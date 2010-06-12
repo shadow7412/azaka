48803fa8f520b3bf6505a0af344d1728712d9163
Shadow <huffy7412@gmail.com>
2010-06-12 20:50:50 +1000

    Created setup file. Run this to get the db working.

--- config/initsetup.php
+++ config/initsetup.php
@@ -1,5 +1,12 @@
-CREATE TABLE  `azaka`.`users` (
-`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+<?php
+include "dbconfig.php";
+
+mysql_connect($db['host'], $db['user'], $db['pass']) or error("The connection to MySQL couldn't be made. Check dbconfig.php.");
+mysql_query("IF NOT EXIST CREATE DATABASE azaka");
+mysql_select_db($db['db']) or error("The database \"".$db['db']."\" couldn't be found. Have you run the installer by any chance..?");
+
+mysql_query("CREATE TABLE  `azaka`.`users` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,in
 `username` VARCHAR( 12 ) NOT NULL ,
 `password` VARCHAR( 20 ) NOT NULL ,
 `access` TINYINT NOT NULL DEFAULT  '0',
@@ -11,4 +18,7 @@ CREATE TABLE  `azaka`.`users` (
 UNIQUE (
 `username`
 )
-) ENGINE = MYISAM 
+) ENGINE = MYISAM");
+
+Header('Location: ..');
+?>
