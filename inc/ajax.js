e99c3002d0fbe34d63261aa5e2a969262f4682d3
Shadow <huffy7412@gmail.com>
2010-06-15 01:49:31 +1000

    Updated ajax to be more modular.
    That hurt. No really...

--- inc/ajax.js
+++ inc/ajax.js
@@ -3,6 +3,7 @@ var anidone = true; //tag to show whether animation has completed
 var currenthash = '';//current #code in browser
 
 function updateContent(url){
+	anidone = false;
 	var params = "";
 	if(url.indexOf('?')!=-1){
 		params = url.substring(url.indexOf('?')+1);
@@ -10,7 +11,6 @@ function updateContent(url){
 	}
 	currenthash = "#"+url.substring(0,url.length-4);
 	window.location.hash = currenthash;
-	anidone = false;
     document.getElementById("loader").innerHTML = '<img src="aesthetics/images/loading.gif" />';
 	$("#content").fadeTo("fast",0, function() {anidone=true;});
 	$("#bottom").fadeTo("fast",0);
@@ -35,7 +35,7 @@ function startautomation(){
 	updatemods();
 }
 function updatemods(){
-	jah("modules","modules");
+	//jah("modules","modules");
 	setTimeout("updatemods();",1000);
 }
 function checkhash(){
@@ -63,7 +63,8 @@ function jah(url,target, params, callback) {
     } else if (window.ActiveXObject) {
         req = new ActiveXObject("Microsoft.XMLHTTP");
         if (req) {
-            req.onreadystatechange = function() {jahDone(target);};
+            if(callback == null) req.onreadystatechange = function() {jahDone(target);};
+			else req.onreadystatechange = function() {eval(callback);};
             req.open("POST", url, true);
 			if(params != null && params != ''){
 				req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -84,7 +85,7 @@ function jahDone(target) {
             results = req.responseText;
             document.getElementById(target).innerHTML = results;
         } else {
-            document.getElementById(target).innerHTML="jah error:\n" + req.statusText;
+            document.getElementById(target).innerHTML="ajax error:\n" + req.statusText;
         }
     }
 }
