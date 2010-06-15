727a9e9dc104e64ad3a57afe8635e7c41c9f9e79
Shadow <huffy7412@gmail.com>
2010-06-15 17:29:10 +1000

    Added icon. (Don't really like it) fixed db.php
    db had a /. Bad db.

--- inc/db.php
+++ inc/db.php
@@ -6,7 +6,7 @@ function error($msg){
 
 class Database{
 	function __construct(){
-		include "/config/dbconfig.php";
+		include "config/dbconfig.php";
 
 		mysql_connect($db['host'], $db['user'], $db['pass']) or error("The connection to MySQL couldn't be made. Make sure MySQL is on, and the username is correct.");
 		mysql_select_db($db['db']) or error("The database \"".$db['db']."\" couldn't be found. Have you run the installer by any chance..?");
