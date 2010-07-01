8ecf44bbd9bc8085a34c6bfacdefb5d01a3a104a
Shadow <huffy7412@gmail.com>
2010-07-01 21:24:37 +1000

    Profile now GETS itself.

--- pages/profile.php
+++ pages/profile.php
@@ -1,15 +1,19 @@
 <?php
 include_once "../include/page.php";
 $p = new Page('profile',1);
-
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
 $p->db->qry("SELECT * FROM users WHERE id='".$p->u->id."'");
 extract($p->db->fetchLast());
-echo "To change your password fill out the password fields - or just leave them be to leave your password be.<br/><br/><form><table>";
-echo "<tr><td>change password</td><td><input type=\"password\" name=\"password\" id=\"password\"/></td></tr>";
-echo "<tr><td>confirm password</td><td><input type=\"password\" name=\"cpassword\" id=\"cpassword\"/></td></tr>";
-echo "<tr><td>first name</td><td><input type=\"text\" name=\"fname\" id=\"fname\" value=\"$firstname\"/></td></tr>";
-echo "<tr><td>last name</td><td><input type=\"text\" name=\"lname\" id=\"lname\" value=\"$lastname\"/></td></tr>";
-echo "<tr><td>email</td><td><input type=\"text\" name=\"email\" id=\"email\" value=\"$email\"/></td></tr>";
-echo "<tr><td><input type=\"submit\" value=\"update\"/></td></tr>";
+echo "To change your password fill out the password fields - or just leave them be to leave your password be.<br/><br/>
+<form name=\"profile\" id=\"profile\" type=\"get\" action=\"pages/profile.php\" onsubmit=\"javascript:sendPost('pages/profile.php?password='+document.profile.password.value+'&firstname='+document.profile.fname.value+'&lastname='+document.profile.lname.value+'&email='+document.profile.email.value);return false;\"><table>
+<tr><td>change password</td><td><input type=\"password\" name=\"password\" id=\"password\"/></td></tr>
+<tr><td>confirm password</td><td><input type=\"password\" name=\"cpassword\" id=\"cpassword\"/></td></tr>
+<tr><td>first name</td><td><input type=\"text\" name=\"fname\" id=\"fname\" value=\"$firstname\"/></td></tr>
+<tr><td>last name</td><td><input type=\"text\" name=\"lname\" id=\"lname\" value=\"$lastname\"/></td></tr>
+<tr><td>email</td><td><input type=\"text\" name=\"email\" id=\"email\" value=\"$email\"/></td></tr>
+<tr><td><input type=\"submit\" value=\"update\"
+')\"/></td></tr>";
 echo "</table></form>";
 ?>
