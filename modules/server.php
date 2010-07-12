a9b1e208a736d783ce78733791bf6c9407a9ef08
shadow7412 <huffy7412@gmail.com>
2010-07-12 18:02:03 +1000

    Moved jquery. Added stats for server.

--- modules/server.php
+++ modules/server.php
@@ -2,8 +2,22 @@
 include_once "../include/module.php";
 
 $m = new Module("server", 2);
-$m->addContent("Upload: <a id=\"mod-server-upload\">...</a><br/>Download:<a id=\"mod-server-download\">...</a>");
+$m->addContent("Upload: <a id=\"mod-server-upload\">...</a><br/>Download:<a id=\"mod-server-download\">...</a><br/><br/>");
+$m->addContent("<strong>time remaining</strong><div id=\"mod-server-timebar\"></div><div id=\"mod-server-time\"></div><br/>");
+$m->addContent("<strong>onpeak remaining</strong><div id=\"mod-server-onpeakbar\"></div><div id=\"mod-server-onpeak\"></div><br/>");
+$m->addContent("<strong>offpeak remaining</strong><div id=\"mod-server-offpeakbar\"></div><div id=\"mod-server-offpeak\"></div><br/>");
 $m->addJs("var modserverxml = (new DOMParser()).parseFromString(grabXML(\"xml/server.php\",\"mod-server\"),\"text/xml\");");
-$m->addJs("document.getElementById('mod-server-upload').innerHTML = modserverxml.getElementsByTagName('upload')[0].childNodes[0].nodeValue;");
-$m->addJs("document.getElementById('mod-server-download').innerHTML = modserverxml.getElementsByTagName('download')[0].childNodes[0].nodeValue;");
+$m->addJs("document.getElementById('mod-server-upload').innerHTML = modserverxml.getElementsByTagName('liveupload')[0].childNodes[0].nodeValue;");
+$m->addJs("document.getElementById('mod-server-download').innerHTML = modserverxml.getElementsByTagName('livedownload')[0].childNodes[0].nodeValue;");
+$m->addJs("$(\"#mod-server-timebar\").progressbar({value: parseInt(modserverxml.getElementsByTagName('time')[0].childNodes[0].nodeValue)});");
+$m->addJs("document.getElementById('mod-server-time').innerHTML = \"days left:\" + modserverxml.getElementsByTagName('time')[0].childNodes[0].nodeValue;");
+$m->addJs("$(\"#mod-server-onpeakbar\").progressbar({value: parseInt(modserverxml.getElementsByTagName('onpercentage')[0].childNodes[0].nodeValue)});");
+$m->addJs("document.getElementById('mod-server-onpeak').innerHTML = \"data used:\" + modserverxml.getElementsByTagName('onpeakused')[0].childNodes[0].nodeValue + \"/\"+ modserverxml.getElementsByTagName('onpeaktotal')[0].childNodes[0].nodeValue;");
+$m->addJs("$(\"#mod-server-offpeakbar\").progressbar({value: parseInt(modserverxml.getElementsByTagName('offpercentage')[0].childNodes[0].nodeValue)});");
+$m->addJs("document.getElementById('mod-server-offpeak').innerHTML = \"data used:\" + modserverxml.getElementsByTagName('offpeakused')[0].childNodes[0].nodeValue + \"/\"+ modserverxml.getElementsByTagName('offpeaktotal')[0].childNodes[0].nodeValue;");
+/*
+data used: 12.11 / 26.00 gb 
+data remaining: 13.89 gb 
+gb/day remaining: 2.62 used: 0.49 
+*/
 ?>
--- xml/server.php
+++ xml/server.php
@@ -2,5 +2,36 @@
 include_once "../include/bandwidth.php";
 $stats = new Bandwidth;
 header ("content-type: text/xml");
-echo "<bandwidth><upload>".$stats->upload."</upload><download>".$stats->download."</download></bandwidth>";
+
+$xml = simplexml_load_file('../../xml/netspace.xml'); //Open (local) NetSpace XML
+$startdate = strtotime($xml["START_DATE"]); //startdate in seconds
+$enddate = strtotime($xml["END_DATE"]); //enddate in seconds
+$ontotal = round($xml->PLAN->LIMIT[0]["MEGABYTES"]/1000,1); //peak total
+$offtotal = round($xml->PLAN->LIMIT[1]["MEGABYTES"]/1000,1); //offpeak total
+$offused = round($xml->TRAFFIC->DATA[0]["DOWNLOADS"]/1000,1); //offpeak used
+$onused = round($xml->TRAFFIC->DATA[1]["DOWNLOADS"]/1000,1); //peakused
+$uploaded = ($xml->TRAFFIC->DATA[0]["UPLOADS"]+$xml->TRAFFIC->DATA[1]["UPLOADS"])/1000; //uploads (both on and off peak)
+$timeleft = date('U',$enddate) - date('U');
+$totaltime = date('U',$enddate) - date('U', $startdate);
+$time = round($timeleft/$totaltime*100,2);
+$daysleft = round((date('U',$enddate) - date('U'))/60/60/24,2);
+$onpercentage = 100 - $onused/$ontotal*100;
+$offpercentage = 100 - $offused/$offtotal*100;
+echo "<bandwidth>
+	<plan>{$xml->PLAN['DESCRIPTION']}</plan>
+	<time>$time</time>
+	<daysleft>$daysleft</daysleft>
+	<onpercentage>$onpercentage</onpercentage>
+	<onpeaktotal>$ontotal</onpeaktotal>
+	<onpeakused>$onused</onpeakused>
+	<offpercentage>$offpercentage</offpercentage>
+	<offpeaktotal>$offtotal</offpeaktotal>
+	<offpeakused>$offused</offpeakused>
+	<uploaded>$uploaded</uploaded>
+	<liveupload>{$stats->upload}</liveupload>
+	<livedownload>{$stats->download}</livedownload>
+</bandwidth>";
+
+
+
 ?>
