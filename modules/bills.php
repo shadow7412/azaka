38c5353222c4b7b20ace9b83605d6fd3cc4e14cd
shadow7412 <huffy7412@gmail.com>
2010-07-18 21:25:51 +1000

    Fixed formatting. bills module is now finished.

--- modules/bills.php
+++ modules/bills.php
@@ -3,7 +3,7 @@ include_once "../include/module.php";
 $m = new Module("bills", 1);
 $nobills=0;
 if($m->db->getSetting('bills_all')){
-	$m->db->qry("SELECT username, SUM(amount) AS amount FROM users, `bills` WHERE uid = users.id AND `paid` = 0 AND `confirmed` = 0 GROUP BY username ORDER BY `uid` ASC");
+	$firstresult = $m->db->qry("SELECT username, SUM(amount) AS amount FROM users, `bills` WHERE uid = users.id AND `paid` = 0 AND `confirmed` = 0 GROUP BY username ORDER BY `uid` ASC");
 	while($row = $m->db->fetchLast()){
 		if($m->u->username == $row['username'])
 			$m->addContent("<strong>");
@@ -11,21 +11,25 @@ if($m->db->getSetting('bills_all')){
 		if($m->u->username == $row['username'])
 			$m->addContent("</strong>");
 	}
-	if($m->db->noLast()!=0) $m->addContent("<br/>"); else $nobills=1;
 	
 	$m->db->qry("SELECT username, SUM(amount) AS amount FROM users, `bills` WHERE uid = users.id AND `paid` = 1 AND `confirmed` = 0 GROUP BY username ORDER BY `uid` ASC");
+	if($m->db->no($firstresult)!=0 && $m->db->noLast()!=0) $m->addContent('<br/>');
 	while($row = $m->db->fetchLast()){
+		if ($nobills==1){$nobills=2;addContent('<br/>');}
 		$m->addContent("{$row['username']} has \${$row['amount']} unconfirmed.<br/>");
 	}
-	if($nobills==1 && $m->db->noLast()==0) $m->addContent("No one has any bills outstanding.");
+	if($m->db->no($firstresult)==0 && $m->db->noLast()==0) $m->addContent("No one has any bills outstanding.");
+
 } else {
-	$m->db->qry("SELECT username, SUM(amount) AS amount FROM users, `bills` WHERE uid = users.id AND `paid` = 0 AND `confirmed` = 0 GROUP BY username ORDER BY `uid` ASC");
+	$m->db->qry("SELECT SUM(amount) AS amount FROM `bills` WHERE uid = '{$m->u->id}' AND `paid` = 0 AND `confirmed` = 0 GROUP BY uid ORDER BY `uid` ASC");
 	if($row = $m->db->fetchLast())
 		$m->addContent("<strong>You have \${$row['amount']} owing.</strong><br/>");
-
-	$m->db->qry("SELECT username, SUM(amount) AS amount FROM users, `bills` WHERE uid = users.id AND `paid` = 1 AND `confirmed` = 0 GROUP BY username ORDER BY `uid` ASC");
-	if($row = $m->db->fetchLast())
-		$m->addContent("<strong>You have \${$row['amount']} unconfirmed.</strong><br/>");
-	if($nobills==1 && $m->db->noLast()==0) $m->addContent("No outstanding bills.");
+	if($m->db->noLast()!=0) $nobills=1;
+	$m->db->qry("SELECT SUM(amount) AS amount FROM `bills` WHERE uid = '{$m->u->id}' AND `paid` = 1 AND `confirmed` = 0 GROUP BY uid ORDER BY `uid` ASC");
+	if($row = $m->db->fetchLast()){
+		if($nobills) $m->addContent("<br/>");
+		$m->addContent("You have \${$row['amount']} unconfirmed.<br/>");
+		}
+	if($nobills==0 && $m->db->noLast()==0) $m->addContent("No outstanding bills.");
 }
 ?>
