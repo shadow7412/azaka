66e82322b34d9a9ebcd4837f3415a0cd6149cb25
shadow7412 <huffy7412@gmail.com>
2010-08-16 18:02:59 +1000

    Vent now works. Ignores channels.

--- modules/ventrilo.php
+++ modules/ventrilo.php
@@ -4,6 +4,9 @@ include_once "../include/ventrilostatus.php";
 $m = new Module("ventrilo", 0);
 
 $m->addJs("var box = document.getElementById('mod-ventrilo');");
-$m->addJs("box.innerHTML = xml.getElementsByTagName('name')[0].childNodes[0].nodeValue;");
-$m->addJs("");
+$m->addJs("box.innerHTML = '<strong>'+xml.getElementsByTagName('name')[0].childNodes[0].nodeValue+'</strong><br/>';");
+$m->addJs("var counter = 0; var users=''; var user='';");
+$m->addJs("while((user = xml.getElementsByTagName('user')[counter++])!=null)");
+$m->addJs("users+=user.childNodes[0].nodeValue+'<br/>';");
+$m->addJs("box.innerHTML+=users;");
 ?>
