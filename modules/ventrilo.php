9f93b1eaef1baebd78d09f2aacca763706743c85
shadow7412 <huffy7412@gmail.com>
2010-08-16 18:03:54 +1000

    Now works. Removed temp variable.

--- modules/ventrilo.php
+++ modules/ventrilo.php
@@ -5,8 +5,7 @@ $m = new Module("ventrilo", 0);
 
 $m->addJs("var box = document.getElementById('mod-ventrilo');");
 $m->addJs("box.innerHTML = '<strong>'+xml.getElementsByTagName('name')[0].childNodes[0].nodeValue+'</strong><br/>';");
-$m->addJs("var counter = 0; var users=''; var user='';");
+$m->addJs("var counter = 0; var user='';");
 $m->addJs("while((user = xml.getElementsByTagName('user')[counter++])!=null)");
-$m->addJs("users+=user.childNodes[0].nodeValue+'<br/>';");
-$m->addJs("box.innerHTML+=users;");
+$m->addJs("box.innerHTML+=user.childNodes[0].nodeValue+'<br/>';");
 ?>
