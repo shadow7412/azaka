9ae3165d6f1b13c9f3a7e43a44e1ecb93652b942
shadow7412 <huffy7412@gmail.com>
2010-08-17 14:46:43 +1000

    Making rss conform.

--- rss/news.php
+++ rss/news.php
@@ -6,22 +6,22 @@ header("Content-Type: application/xml; charset=ISO-8859-1");
 echo "<rss version=\"2.0\"> 
 <channel> 
 <title>news - azaka</title> 
-<link></link> 
+<link>{$_SERVER['SERVER_NAME']}/{$_SERVER['REQUEST_URI']}</link> 
 <description>azaka news</description> 
 <language>en-uk</language>  
-<lastBuildDate>".time()."</lastBuildDate> 
+<lastBuildDate>".date('DATE_RFC822',time())."</lastBuildDate> 
 <ttl>60</ttl>";
 
 $u->db->qry("SELECT * FROM news");
 while($item = $u->db->fetchLast()){
-	echo"
+	echo "
 	<item> 
 	<title>{$item['title']} by {$item['uid']}</title> 
-	<link></link> 
+	<link>{$_SERVER['SERVER_NAME']}{$_SERVER['REQUEST_URI']}</link> 
 	<pubDate>{$item['time']}</pubDate> 
 	<description><![CDATA[ {$item['content']}]]></description> 
 	</item>\n";
 }
 echo "</channel> 
-</rss>"
+</rss>";
 ?>
