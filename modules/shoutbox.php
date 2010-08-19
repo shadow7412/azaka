58657dfff3a95f7947fb7a70a72762902799382c
shadow7412 <huffy7412@gmail.com>
2010-08-19 15:19:32 +1000

    Set shoutbox to refresh, added more help for gautam

--- modules/shoutbox.php
+++ modules/shoutbox.php
@@ -8,4 +8,6 @@
 //add the form using addContent() - verify a message is printed. Maybe do not show form unless a user is logged in
 
 //add the messages into the place holder using javascript > addJs()
+//You will want to see another module file (such as links) to figure
+//this out
 ?>
