6775d16fa046027930e1f195a82b44183721bf9f
shadow7412 <huffy7412@gmail.com>
2010-10-04 20:30:41 +1100

    Deleted commenting

--- include/userobject.php
+++ include/userobject.php
@@ -68,40 +68,6 @@ class UserObject {
 			$this->access = 0;
 		}		
 	}
-/*
-	function updateUser(){
-	//first pull in any cookie info
-		if(isset($_COOKIE['azaka_username']) && isset($_COOKIE['azaka_password'])){
-			$this->username = $_COOKIE['azaka_username'];
-			$this->password = $_COOKIE['azaka_password'];
-		}
-	//if cookie info is existent and correct, log user in. if not, destroy stuff.
-		if(isset($this->username)){
-			if($result = $this->db->fetch(
-				$this->db->qry("SELECT * FROM users WHERE username='".$this->username."' AND password = '".$this->password."' AND disabled=0")
-			)){
-				$this->id = $result['id'];
-				$this->access = $result['access'];
-				$this->firstname = $result['firstname'];
-				$this->lastname = $result['lastname'];
-				$this->dob = $result['dob'];
-				$this->billable = $result['billable'];
-				$this->skin = $result['skin'];
-				$this->email = $result['email'];
-				$this->db->qry("UPDATE  `users` SET  `lastactive` = NOW( ) WHERE  `users`.`id` ={$this->id};");
-			} else {
-				unset($this->username);
-				$this->changed = true;
-			}
-		}
-	//if user did not successfully log in, log in a pseudo guest account
-		if(!isset($this->username)){
-			$this->username = "guest";
-			$this->skin = 1;
-			$this->access = 0;
-		}
-	}
-	*/
 	function invalidateSession(){
 		setcookie("azaka_user",'');
 	}
