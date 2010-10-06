b49c2c48270e38b95c60b349911fdcc9d24bb427
shadow7412 <huffy7412@gmail.com>
2010-10-07 09:51:31 +1100

    Removed error script in shoutbox.

--- modules/links.php
+++ modules/links.php
@@ -6,5 +6,5 @@ $m = new Module("links", 0);
 $m->addContent("<ul id=\"mod-links-content\"></ul>");
 $m->addJs("var links = document.getElementById('mod-links-content'); links.innerHTML = '';var count = 0; var link = null;");
 $m->addJs("while((link = xml.getElementsByTagName('link')[count++]) != undefined)");
-$m->addJs("links.innerHTML += '<li><a href=\"'+link.childNodes[1].childNodes[0].nodeValue+'\">' + link.childNodes[0].childNodes[0].nodeValue + '</a></li>'");
+$m->addJs("links.innerHTML += '<li class=\"linklist\"><a href=\"'+link.childNodes[1].childNodes[0].nodeValue+'\">' + link.childNodes[0].childNodes[0].nodeValue + '</a></li>'");
 ?>
