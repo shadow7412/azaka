8d6f8ba7f0ab9e202d1c3d01a3aa28d572ff805f
deathkiller <deathkiller@deathkiller-laptop.(none)>
2010-10-20 19:44:23 +1100

    pages/shoutbox had the wrong code pushed..so I corrected it and pushing the right code now..

--- pages/shoutbox.php
+++ pages/shoutbox.php
@@ -15,16 +15,13 @@ if(isset($_GET['delete']) && isset($_GET['idNumber'])){
     if($u->id == $_GET['idNumber'] || $u->canAccess(2)) $p->db->qry("DELETE FROM shoutbox WHERE id =".$_GET['delete']);
 }
 
-$p->db->qry("SELECT users.username as username, shoutbox.time as time, shoutbox.message as message FROM users,shoutbox WHERE shoutbox.uid = users.id ORDER BY time DESC");
+$p->db->qry("SELECT shoutbox.id as id, shoutbox.uid as uid, users.username as username, shoutbox.time as time, shoutbox.message as message FROM users,shoutbox WHERE shoutbox.uid = users.id ORDER BY time DESC");
 echo "<div id=\"accordion\">";
 
 while($row = $p->db->fetchLast()){
    echo "<h3><a>".$row['username']." ".$row['time']."</a></h3>
              <div>".$row['message'];
-   if($u->id == $row['uid'] || $u->canAccess(2)) echo "<a
-href='#shoutbox&delete={$row['id']}&idNumber={$row['uid']}'
-style=\"float: right; margin-right:4%;\"><input type= 'submit'
-class=\"ui-button ui-widget ui-state-default ui-corner-all\"name='Delete' value='Delete'></a>"; 
+   if($u->id == $row['uid'] || $u->canAccess(2)) echo "<a href='#shoutbox&delete={$row['id']}&idNumber={$row['uid']}' style=\"float: right; margin-right:4%;\"><input type= 'submit' class=\"ui-button ui-widget ui-state-default ui-corner-all\" name='Delete' value='Delete'></a>"; 
    echo "</div>";
 }
 
