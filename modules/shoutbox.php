dfaf34671f51578ec8f934bf54b3c70e270c9324
deathkiller <deathkiller@deathkiller-laptop.(none)>
2010-10-20 22:42:06 +1100

    Changed the module look of shoutbox, moved the message field on top

--- modules/shoutbox.php
+++ modules/shoutbox.php
@@ -6,11 +6,6 @@ include_once "../include/userobject.php";
 $m = new Module("shoutbox", 0);
 $u = new UserObject;
 
-$m->addContent("<div id=\"mod-shoutbox-contents\"></div>");
-$m->addJs("var shoutbox = document.getElementById('mod-shoutbox-contents'); shoutbox.innerHTML = ''; var count = 0;");
-$m->addJs("while((message = xml.getElementsByTagName('message')[count++]) != undefined)");
-$m->addJs("shoutbox.innerHTML += '<p><b>' + message.childNodes[0].childNodes[0].nodeValue + '</b> ' + message.childNodes[1].childNodes[0].nodeValue + '<br />' + message.childNodes[2].childNodes[0].nodeValue + '</p>'");
-
 if($u->canAccess(1)){
    $m->addContent("<form id=\"mod-shoutbox-form\" name=\"Shoutblog\" onsubmit=\"if(document.getElementById('mod-shoutbox-form').message.value == ''){
                      errorMsg('Please enter a message in the field before pressing enter..'); return false;}
@@ -24,5 +19,10 @@ if($u->canAccess(1)){
                   </table>
                   </form>");
 }
+
+$m->addContent("<div id=\"mod-shoutbox-contents\"></div>");
+$m->addJs("var shoutbox = document.getElementById('mod-shoutbox-contents'); shoutbox.innerHTML = ''; var count = 0;");
+$m->addJs("while((message = xml.getElementsByTagName('message')[count++]) != undefined)");
+$m->addJs("shoutbox.innerHTML += '<p><b>' + message.childNodes[0].childNodes[0].nodeValue + '</b> ' + message.childNodes[1].childNodes[0].nodeValue + '<br />' + message.childNodes[2].childNodes[0].nodeValue + '</p>'");
 
 ?>
