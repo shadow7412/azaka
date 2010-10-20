a0f3bf8db02c4a5dbcc189d9fe32c2b189832105
deathkiller <deathkiller@deathkiller-laptop.(none)>
2010-10-20 22:02:51 +1100

    Shoutbox is complete AGAIN!!! WOOOOhhhooooo....

--- pages/shoutbox.php
+++ pages/shoutbox.php
@@ -11,17 +11,24 @@ if(isset($_GET['message'])){
    $p->db->qry("INSERT INTO shoutbox VALUES(default,".$u->id.",default,\"".$_GET['message']."\")");
 }
 
-if(isset($_GET['delete']) && isset($_GET['idNumber'])){
-    if($u->id == $_GET['idNumber'] || $u->canAccess(2)) $p->db->qry("DELETE FROM shoutbox WHERE id =".$_GET['delete']);
+if(isset($_GET['shout_id'])){
+   $p->db->qry("SELECT shoutbox.uid as idNumber FROM shoutbox WHERE id =".$_GET['shout_id']);
+   while($number = $p->db->fetchLast()){
+
+      if($u->id == $number['idNumber'] || $u->canAccess(2)) 
+         $p->db->qry("DELETE FROM shoutbox WHERE id =".$_GET['shout_id']);
+}
 }
 
-$p->db->qry("SELECT shoutbox.id as id, shoutbox.uid as uid, users.username as username, shoutbox.time as time, shoutbox.message as message FROM users,shoutbox WHERE shoutbox.uid = users.id ORDER BY time DESC");
+$p->db->qry("SELECT shoutbox.id as shout_id, shoutbox.uid as idNumber, users.username as username, shoutbox.time as time, shoutbox.message as message FROM users,shoutbox WHERE shoutbox.uid = users.id ORDER BY time DESC");
 echo "<div id=\"accordion\">";
 
 while($row = $p->db->fetchLast()){
+
    echo "<h3><a>".$row['username']." ".$row['time']."</a></h3>
              <div>".$row['message'];
-   if($u->id == $row['uid'] || $u->canAccess(2)) echo "<a href='#shoutbox&delete={$row['id']}&idNumber={$row['uid']}' style=\"float: right; margin-right:4%;\"><input type= 'submit' class=\"ui-button ui-widget ui-state-default ui-corner-all\" name='Delete' value='Delete'></a>"; 
+
+   if($u->id == $row['idNumber'] || $u->canAccess(2)) echo "<div style=\"float: right; margin-right:4%;\"><input type=\"button\" OnClick=\"grabContent('shoutbox', 'shout_id=".$row['shout_id']."')\" class=\"ui-button ui-widget ui-state-default ui-corner-all\" value=\"Delete\"/></div>"; 
    echo "</div>";
 }
 
